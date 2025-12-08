cd3b3a1413edd47de2d68a7701d1268b7429e8fd
Akshay Veesam <67137623+akshay9056@users.noreply.github.com>
2025-12-08 19:58:50 +0530

    Restrict CORS allowed origins to frontend domain
    Updated CORS configuration to restrict allowed origins to the specified frontend domain.

--- src/main/java/com/avangrid/gui/avangrid_backend/config/CorsConfig.java
+++ src/main/java/com/avangrid/gui/avangrid_backend/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOrigins(FRONTEND_DOMAIN)
                         .allowedMethods("GET", "POST")
                         .allowedHeaders("*")
                         .allowCredentials(true)
@@ -26,6 +26,7 @@ public class CorsConfig {
         };
     }
 }
+
 
 
 
